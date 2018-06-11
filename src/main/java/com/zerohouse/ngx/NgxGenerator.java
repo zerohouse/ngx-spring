03318e6925346237a51cc4f15dc9f8ebbaa63fd8
parks <parksungho86@gmail.com>
2018-06-12 07:57:06 +0900

    1.0.8
    EnumArrays

--- src/main/java/com/zerohouse/ngx/NgxGenerator.java
+++ src/main/java/com/zerohouse/ngx/NgxGenerator.java
@@ -82,7 +82,7 @@ public class NgxGenerator {
             TsGenerator ngxModule = new TsGenerator("NgxSpringModule",
                     "import {NgModule} from '@angular/core';\n" +
                             "import {HttpClientModule} from '@angular/common/http';\n" +
-                            "import {ApiService} from './api.service';\n"+
+                            "import {ApiService} from './api.service';\n" +
                             "import {ApiHttp} from './api.http';");
             ngxModule.head = "@NgModule({\n" +
                     "  imports: [HttpClientModule],\n" +
@@ -132,6 +132,8 @@ public class NgxGenerator {
                     "  }\n" +
                     "}\n", "utf8");
             ngxModule.saveResult(outputPath);
+            String enums = this.types.stream().filter(type -> type.getClass().isAssignableFrom(Enum.class)).map(type -> this.enumValueArray((Class<? extends Enum>) type)).collect(Collectors.joining("\n"));
+            FileUtils.write(new File(outputPath + "/enum.values.ts"), enums, "utf8");
             new TypeScriptGenerator(settings).generateTypeScript(
                     Input.from(this.types.toArray(new Type[]{})),
                     Output.to(new File(outputPath + "/api.model.d.ts")));
@@ -140,6 +142,11 @@ public class NgxGenerator {
             e.printStackTrace();
         }
 
+    }
+
+    public String enumValueArray(Class<? extends Enum> clazz) {
+        String name = CaseFormat.UPPER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, clazz.getSimpleName()) + "_VALUES";
+        return String.format("export const %s = [%s];\n", name, Arrays.stream(clazz.getEnumConstants()).map(o -> String.format("'%s'", o)).collect(Collectors.joining(", ")));
     }
 
 
